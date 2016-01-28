11bb202731cfb319ea12435b5034f8ad784d65ff
Anthony Hoang <ant.hoang93@gmail.com>
2016-01-27 21:24:13 -0800

    Create filter function for events

--- app/assets/javascripts/stores/event_store.js
+++ app/assets/javascripts/stores/event_store.js
@@ -7,8 +7,37 @@
     _events = events;
   };
 
-  var filterFutureEvents = function (event) {
+  var filterFutureEvents = function (events) {
+    var filtered = [];
     var today = new Date().toJSON().slice(0,10);
+    // '2016-01-28'
+    today = today.split('-');
+    var todayYear = parseInt(today[0]);
+    var todayMonth = parseInt(today[1]);
+    var todayDay = parseInt(today[2]);
+
+    for (var i = 0; i < events.length; i++) {
+      var start_time = parseDate(events[i].start_time);
+      var startYear = parseInt(start_time[0]);
+      var startMonth = parseInt(start_time[1]);
+      var startDay = parseInt(start_time[2]);
+
+      if (startYear >= todayYear) {
+        if (startMonth >= todayMonth){
+          if (startDay >= todayDay){
+            console.log("push");
+            filtered.push(events[i]);
+          }
+        }
+      }
+    }
+    debugger
+    return filtered;
+
+  };
+
+  var parseDate = function (date) {
+    return date.split('T')[0].split('-');
   };
 
   root.EventStore = $.extend({}, EventEmitter.prototype, {
@@ -27,7 +56,7 @@
     dispatcherId: AppDispatcher.register(function(action){
       switch (action.actionType) {
         case EventConstants.EVENTS_RECEIVED:
-          resetEvents(action.events);
+          filterFutureEvents(action.events);
           root.EventStore.emit(CHANGE_EVENT);
           break;
       }
