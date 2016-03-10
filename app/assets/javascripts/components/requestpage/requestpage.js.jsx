e3b3162bb16ff5646b5755b847b3a994d791e70d
Anthony Hoang <ant.hoang93@gmail.com>
2016-03-09 16:37:12 -0800

    Create layout for request form

--- app/assets/javascripts/components/requestpage/requestpage.js.jsx
+++ app/assets/javascripts/components/requestpage/requestpage.js.jsx
@@ -1,15 +1,55 @@
 var RequestPage = React.createClass({
   mixins : [ReactRouter.History],
+  getInitialState : function () {
+    return ({
+      name : "",
+      place : "",
+      date : ""
+    });
+  },
+
+  _inputChange : function (e) {
+    switch (e.target.id){
+      case "name" :
+        this.setState({
+          name : e.target.value
+        });
+        break;
+      case "place":
+        this.setState({
+          place : e.target.value
+        });
+        break;
+      case "date":
+        this.setState({
+          date : e.target.value
+        });
+        break;
+    }
+  },
+
+  _handleSubmit : function (){
+
+  },
+  
   render : function () {
     return (
       <div id="requestContainer">
         <div className="formContainer">
-          <form>
-          <h1>Fill out the details of the event below</h1>
-            <input placeholder="Hello"></input>
-            <input placeholder="Hello"></input>
-            <input placeholder="Hello"></input>
-            <input placeholder="Hello"></input>
+          <form onSubmit={this._handleSubmit}>
+            <h1>Can't find an event?</h1>
+            <h2>Fill out the details of the event below</h2>
+            <h3>We will review and update shortly</h3>
+            <input placeholder="Event Name" onChange={this._inputChange}
+              value={this.state.name}
+              id="name"/>
+            <input placeholder="Event Location" onChange={this._inputChange}
+              value={this.state.place}
+              id="place"/>
+            <input placeholder="Event Date" onChange={this._inputChange}
+              value={this.state.date}
+              id="date"/>
+            <button className="requestButton">Submit Request</button>
           </form>
         </div>
       </div>
